a2a2ba4c6bca416b57cd3037180cdf894e87fa19
Toomy115 <tomascicenia@hotmail.com>
2022-04-05 21:34:07 -0300

    Update

---
+++ ProyectoBase/Game/ControlManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class ControlManager
+    {
+        private Player player;
+
+        public ControlManager()
+        {
+            Start();
+        }
+        private void Start()
+        {
+            player = Program.ObtenerJugador();
+        }
+        public void CheckInput()
+        {
+            if (Engine.GetKey(Keys.W))
+            {
+                player.MoveY = player.MoveY - player.GetSpeed * Program.GetDeltaTime;
+            }
+            if (Engine.GetKey(Keys.D))
+            {
+                player.MoveX = player.MoveX + player.GetSpeed * Program.GetDeltaTime;
+            }
+            if (Engine.GetKey(Keys.S))
+            {
+                player.MoveY = player.MoveY + player.GetSpeed * Program.GetDeltaTime;
+            }
+            if (Engine.GetKey(Keys.A))
+            {
+                player.MoveX = player.MoveX - player.GetSpeed * Program.GetDeltaTime;
+            }
+            if (Engine.GetKey(Keys.SPACE))
+            {
+                player.Shoot();
+            }
+        }
+            
+
+        
+    }
+}
